7abd4aafe7a4ca38fa84513645972aa0a8724cba
Joseph Sarpong <Joseph-Sarpong@hotmail.com>
2021-06-25 12:02:45 +0000

    added 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9) 
+			{
+				_putchar(48 + (i / 10));
+			}
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+		j++;
+	}
+}
