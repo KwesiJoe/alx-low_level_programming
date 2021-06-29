a7d4fb8ab9b07a3cf6d55b264a0e34401c17e4f6
Joseph Sarpong <Joseph-Sarpong@hotmail.com>
2021-06-29 17:19:44 +0000

    added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - a function that prints a string, followed by a new line, to
+ * stdout.
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	while (count > 0)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+	_putchar('\n');
+
+}
