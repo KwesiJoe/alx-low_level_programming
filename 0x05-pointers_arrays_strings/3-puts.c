50ff9eec2463e2b2378b3d86769e7ca8dcfc5cd8
Joseph Sarpong <Joseph-Sarpong@hotmail.com>
2021-06-29 15:58:51 +0000

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int letter = 0;
+	
+	while (*(str + letter) != '\0')
+	{
+		_putchar(*(str + letter));
+		letter++;
+	}
+	_putchar('\n');
+}
