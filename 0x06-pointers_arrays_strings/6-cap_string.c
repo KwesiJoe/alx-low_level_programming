02e5889d55bd8d3ca75998c56ae34d709feca479
Joseph Sarpong <Joseph-Sarpong@hotmail.com>
2021-07-05 16:38:00 +0000

    modified 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,9 +8,14 @@
 
 char *cap_string(char *s)
 {
-	int idx = 0;
+	int idx = 1;
 	int sep;
 	int seperators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*s >= 97 && *s <= 122)
+		{
+			*s = *s - 32;
+		}
 
 	while (*(s + idx) != '\0')
 	{
