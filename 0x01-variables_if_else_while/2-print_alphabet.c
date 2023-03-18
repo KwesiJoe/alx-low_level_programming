5ae4071266037b3420b12b9f67c4938e2f33f6f5
Joseph Sarpong <joseph-sarpong@hotmail.com>
2023-03-18 13:53:52 +0000

    small letter alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	for (char i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
