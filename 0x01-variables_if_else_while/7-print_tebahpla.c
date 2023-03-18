e3f25402d733b3555905d00cbf2ca0dae1962186
Joseph Sarpong <joseph-sarpong@hotmail.com>
2023-03-18 15:06:06 +0000

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
